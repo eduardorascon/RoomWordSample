6fa9315e05dbe49a5f3d05c7d66a551e87e84505
eduardorascon <eduardo.rascon@gmail.com>
2017-12-07 12:12:34 -0600

    RecyclerView added to the Main activity.

--- app/src/main/java/com/eduardorascon/roomwordsample/roomwordsample/MainActivity.java
+++ app/src/main/java/com/eduardorascon/roomwordsample/roomwordsample/MainActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
