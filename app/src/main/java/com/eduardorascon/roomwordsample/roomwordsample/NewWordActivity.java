d4a6cb99ca72781d2ea46e64344aade3b08805f5
eduardorascon <eduardo.rascon@gmail.com>
2017-12-08 19:11:54 -0600

    New activity NewWordActivity created.

---
+++ app/src/main/java/com/eduardorascon/roomwordsample/roomwordsample/NewWordActivity.java
@@ -0,0 +1,12 @@
+package com.eduardorascon.roomwordsample.roomwordsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
